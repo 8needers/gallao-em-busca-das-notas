95842433682dfefb100325b004330060ea1ee983
Mauricio Cardozo <dedigmac1@Mauricios-Mac-mini.local>
2015-06-26 13:13:11 -0300

    Interface completa do menu principal e adição de arquivos do jogo

---
+++ Assets/Scripts/Interface/FadeOutUIImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOutUIImage : MonoBehaviour {
+
+	public float tempoAntesDoFade, tempoDeFade;
+
+	void Start () {
+		StartCoroutine(DelayedStart());
+	}
+
+	IEnumerator DelayedStart(){
+		yield return new WaitForSeconds(tempoAntesDoFade);
+		this.GetComponent<Image>().CrossFadeAlpha(0, tempoDeFade, true);
+		StartCoroutine(DisableGameObject());
+	}
+
+	IEnumerator DisableGameObject(){
+		yield return new WaitForSeconds(tempoDeFade);
+		this.gameObject.SetActive(false);
+	}
+
+}
--- Assets/Scripts/Interface/UserInterfaceActions.cs
+++ Assets/Scripts/Interface/UserInterfaceActions.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class UserInterfaceActions : MonoBehaviour {
 	// É meio perda de tempo ficar montando várias classes pra fazer uma interface tão simples/descartável, acho melhor concentrar tudo aqui
 
-	public GameObject principal, creditos;
+	public GameObject principal, creditos, botaoSomHabilitado, botaoSomDesabilitado;
+	bool audioHabilitado = true;
 
 	#region Ações dos botões do menu principal
 	/// <summary>
@@ -26,6 +27,31 @@ public class UserInterfaceActions : MonoBehaviour {
 		creditos.SetActive(true); 
 		principal.SetActive(false);
 	}
+
+	public void AlternarSom(){
+		if (audioHabilitado) {
+			DesativarSom();
+		} else {
+			AtivarSom();
+		}
+	}
+	
+	void DesativarSom(){
+		AudioListener.volume = 0;
+		audioHabilitado = false;
+		botaoSomHabilitado.SetActive(false);
+		botaoSomDesabilitado.SetActive(true);
+		PlayerPrefs.SetInt("som", 0);
+	}
+	
+	void AtivarSom(){
+		AudioListener.volume = 1;
+		audioHabilitado = true;
+		botaoSomHabilitado.SetActive(true);
+		botaoSomDesabilitado.SetActive(false);
+		PlayerPrefs.SetInt("som", 1);
+	}
+
 	#endregion 
 
 	#region Ações dos botões do menu de configurações
