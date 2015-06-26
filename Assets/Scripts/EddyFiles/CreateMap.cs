8dba0eca6661808c2134529100dfd716a6950208
Mauricio Cardozo <dedigmac1@Mauricios-Mac-mini.local>
2015-06-26 14:14:02 -0300

    Adição dos arquivos do Eddy

---
+++ Assets/Scripts/EddyFiles/CreateMap.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateMap : MonoBehaviour
+{
+
+		//==========GAME OBJECT
+		public List<GameObject> tileMap;
+
+		//==========IMAGES
+		List<Sprite> imageList = new List<Sprite> ();
+
+		//==========PREFABS
+		public GameObject tilePrefab;
+		public GameObject wumpusPrefab;
+		public GameObject testPrefab;
+		public GameObject waterPrefab;
+
+		//==========VARIABLES
+		/// <summary>
+		/// Map Size: Size of Mapa in coordinates X,Y
+		/// </summary>
+
+		public int mapSize;
+
+		// Use this for initialization
+		void Start ()
+		{
+				SetMap ();
+		}
+
+		//======
+		//CREATE THE MAP
+		//======
+		void SetMap ()
+		{
+				tileMap = new List<GameObject> ();
+				for (int i = 0; i< mapSize; i++) {
+						for (int j = 0; j< mapSize-1; j++) {
+								tileMap.Add (Instantiate (tilePrefab, 
+				                                new Vector3 (this.gameObject.transform.position.x + i, this.gameObject.transform.position.y + j, 0),
+				                                Quaternion.identity) as GameObject);
+						}
+				}
+		RandomizeElements ();
+		}
+
+	void RandomizeElements(){
+		Wumpus mWumpus = new Wumpus ();
+		mWumpus = ((GameObject)Instantiate (wumpusPrefab, new Vector3 (Random.Range (2, 10), Random.Range (2, 10), 0f), Quaternion.Euler(new Vector3(0,0,0)))).GetComponent<Wumpus>();
+
+		Test mTest = new Test ();
+		mTest = ((GameObject)Instantiate (testPrefab, new Vector3 (Random.Range (2, 10), Random.Range (2, 10), 0f), Quaternion.Euler(new Vector3(0,0,0)))).GetComponent<Test>();
+
+		Water mWater = new Water ();
+		mWater = ((GameObject)Instantiate (waterPrefab, new Vector3 (Random.Range (2, 10), Random.Range (2, 10), 0f), Quaternion.Euler(new Vector3(0,0,0)))).GetComponent<Water>();
+
+	}
+
+		
+}
